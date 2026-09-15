ae026a4a5af85d189e5445280bddeff61da0d9e4
koorosh-sly <koorosh.soleimani@gmail.com>
2026-09-16 02:40:56 +0330

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function startCountdown() {
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60)) / (1000 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     const elDays = document.getElementById('days');
